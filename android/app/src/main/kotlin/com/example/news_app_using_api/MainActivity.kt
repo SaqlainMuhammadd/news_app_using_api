e3ab1a24eed22490980de70037954eb2e8012b54
MuhammadSaqlain143 <miansaqlainqureshi@gmail.com>
2024-01-01 04:14:15 -0800

    New repo

---
+++ android/app/src/main/kotlin/com/example/news_app_using_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.news_app_using_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({super.key});
+
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.red,
+      ),
+    );
+  }
+}
